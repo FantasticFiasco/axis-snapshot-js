be9b3582d4bc3719193bf324988d7a5d5fd73647
FantasticFiasco <mattias.kindborg@gmail.com>
2020-10-16 23:53:48 +0200

    wip

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./axis-image.cjs.production.min.js')
-} else {
-  module.exports = require('./axis-image.cjs.development.js')
-}
