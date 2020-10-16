6e073c413a88808c451d41ee6aa50c5762f90453
FantasticFiasco <mattias.kindborg@gmail.com>
2020-10-17 00:13:25 +0200

    wip

--- src/requests/Request.ts
+++ src/requests/Request.ts
@@ -1,7 +1,7 @@
 import * as rp from 'request-promise-native';
 import * as errors from 'request-promise-native/errors';
 import { Connection } from '..';
-import { RequestError, UnauthorizationError } from '../errors';
+import { RequestError, UnauthorizedError } from '../errors';
 
 export abstract class Request {
     protected constructor(protected readonly connection: Connection) {}
@@ -31,7 +31,7 @@ export abstract class Request {
     private handleStatusCodeError(error: Error) {
         if (error instanceof errors.StatusCodeError) {
             if (error.statusCode === 401) {
-                throw new UnauthorizationError();
+                throw new UnauthorizedError();
             }
 
             throw new RequestError(error.message, error.statusCode, undefined, error.error, error.response);
