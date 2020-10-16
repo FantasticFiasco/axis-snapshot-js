d9d76aaf8829de9ffb4bcb6dd55f15fec3cad04b
Mattias Kindborg @FantasticFiasco <FantasticFiasco@users.noreply.github.com>
2020-10-17 00:16:02 +0200

    feat: jpeg snapshot (#1)

---
+++ src/Snapshot.ts
@@ -0,0 +1,25 @@
+import { Connection } from './Connection';
+import { JpegRequest } from './requests/JpegRequest';
+
+/**
+ * Class responsible for getting snapshots from a camera.
+ */
+export class Snapshot {
+    /**
+     * Initializes a new instance of the class.
+     * @param connection The connection to the device.
+     */
+    constructor(private readonly connection: Connection) {}
+
+    /**
+     * Takes a JPEG snapshot from the camera.
+     * @throws {UnauthorizedError} User is not authorized to perform operation.
+     * @throws {RequestError} Request failed.
+     */
+    public async jpeg(): Promise<Buffer> {
+        const request = new JpegRequest(this.connection);
+        const response = await request.send();
+
+        return response;
+    }
+}
