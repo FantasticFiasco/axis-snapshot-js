478fb011f932beaf94d81d2dddbc51707138af60
FantasticFiasco <mattias.kindborg@gmail.com>
2020-10-18 09:11:34 +0200

    wip

--- src/sample.ts
+++ src/sample.ts
@@ -1,5 +1,5 @@
 import { writeFileSync } from 'fs';
-import { Connection, Protocol, Snapshot } from './';
+import { Connection, Protocol, Snapshot } from '.';
 
 const connection = new Connection(Protocol.Http, '192.168.1.130', 80, 'root', '8l1QyDhbZGprv');
 const snapshot = new Snapshot(connection);
