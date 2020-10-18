3f11dd9885dcf67504252547310eab4ec6be9dd3
FantasticFiasco <mattias.kindborg@gmail.com>
2020-10-18 09:10:40 +0200

    wip

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,4 @@ import { Connection, Protocol, Snapshot } from './';
 const connection = new Connection(Protocol.Http, '192.168.1.130', 80, 'root', '8l1QyDhbZGprv');
 const snapshot = new Snapshot(connection);
 
-snapshot.jpeg().then((image: Buffer) => writeFileSync('snapshot.jpeg', image));
+snapshot.jpeg({ compression: 20, rotation: 180 }).then((image: Buffer) => writeFileSync('snapshot.jpeg', image));
